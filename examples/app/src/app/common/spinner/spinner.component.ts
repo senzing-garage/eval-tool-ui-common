57f0c1d40866a832333a25b0e657b6a2ddd9e6ef
Ara Winters <ara.winters@gmail.com>
2025-08-23 17:20:58 -0700

    #8 updated style prototype resurrected WIP

--- examples/app/src/app/common/spinner/spinner.component.ts
+++ examples/app/src/app/common/spinner/spinner.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SpinnerService } from '../../services/spinner.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.scss']
+  styleUrls: ['./spinner.component.scss'],
+  imports: [CommonModule]
 })
 export class SpinnerComponent implements OnInit {
   public _active = true;
