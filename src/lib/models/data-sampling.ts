e5fa99fc926c30de99d823b4c95402546708c8b6
Ara Winters <ara.winters@gmail.com>
2026-01-12 13:21:05 -0800

    review sample table WIP

---
+++ src/lib/models/data-sampling.ts
@@ -0,0 +1,124 @@
+import { SzSdkEntityResponse, SzSdkRelatedEntity, SzSdkResolvedEntity } from '../models/grpc/engine';
+import { SzBoundType } from '../models/statistics/szBoundType';
+import { SzRelationType } from '../models/statistics/szRelationType';
+
+export interface SzSampleSetEntity {
+    entity?: SzSdkResolvedEntity;
+    /**
+     * The array of RelatedEntity instances describing the possible matches, discovered relationships, and disclosed relationships.
+     */
+    relatedEntities?: Array<SzSdkRelatedEntity>;
+}
+export interface SzSampleSetRelation {
+    entity?: SzSdkResolvedEntity;
+    /**
+     * The array of RelatedEntity instances describing the possible matches, discovered relationships, and disclosed relationships.
+     */
+    relatedEntity?: SzSdkRelatedEntity;
+    matchType?: SzRelationType;
+    /**
+     * The match key describing what features matched between the first record in the resolved entity and this record.
+     */
+    matchKey?: string;
+    /**
+     * The code identifying the resolution rule that related the entities to one another.
+     */
+    principle?: string;
+}
+export interface SzSampleSetRelationsPage { 
+    /**
+     * The relationship bound value that contains two (2) entity ID values separated by a colon (e.g.: `1000:5005`).  The first entity ID value identifies the first entity in the relationship and the second entity  ID value identifies the related entity in the relationship.
+     */
+    bound: string;
+    boundType: SzBoundType;
+    /**
+     * The requested page size representing the maximum number of  `SzRelation`'s' that were included in the page.
+     */
+    pageSize: number;
+    /**
+     * The requested sample size representing the number of `SzRelation`'s to be randomly selected from the page of results.
+     */
+    sampleSize?: number;
+    /**
+     * The minimum relation value of the returned results.  This is encoded the same as the `bound` value with two (2) entity ID values separated by a colon (e.g.: `1000:5005`).  The first entity ID value identifies the least value of first entity in the relationship and the second entity ID value identifies the least value of those entity ID's related to the first entity. **NOTE:** This field is absent or `null` if there are no results.
+     */
+    minimumValue?: string;
+    /**
+     * The maximum relation value of the returned results.  This is encoded the same as the `bound` value with two (2) entity ID values separated by a colon (e.g.: `1000:5005`).  The first entity ID value identifies the greatest value of first entity in the relationship and the second entity ID value identifies the greatest value of those entity ID's related to the first  entity.  **NOTE:** This field is absent or `null` if there are no results.
+     */
+    maximumValue?: string;
+    /**
+     * The minimum relation value of the entire relations page.  This will be the same as `minimumValue` if `sampleSize` was not  specified, however, if `sampleSize` was specified then this  will be the minimum relation value of all the candidate relations on the page that were used for random sample selection even if that relation was not randomly selected.  This is encoded the same as the `bound` value with two (2) entity ID values separated by a colon (e.g.: `1000:5005`).  The first entity ID value identifies the least value of first entity in the relationship and the second entity ID value identifies the least value of those entity ID's related to the first entity. **NOTE:** This field is absent or `null` if there are no results.
+     */
+    pageMinimumValue?: string;
+    /**
+     * The maximum relation value of the entire relations page.  This will be the same as `maximumValue` if `sampleSize` was not  specified, however, if `sampleSize` was specified then this  will be the maximum relation value of all the candidate relations on the page that were used for random sample selection even if that relation was not randomly selected.  This is encoded the same as the `bound` value with two (2) entity ID values separated by a colon (e.g.: `1000:5005`).  The first entity ID value identifies the greatest value of first entity in the relationship and the second entity ID value identifies the greatest value of those entity ID's related to the first entity. **NOTE:** This field is absent or `null` if there are no results.
+     */
+    pageMaximumValue?: string;
+    /**
+     * The total number of relationships representing the set of all  possible results across all pages.
+     */
+    totalRelationCount: number;
+    /**
+     * The number of relationships in the set that exist on pages before this page.
+     */
+    beforePageCount: number;
+    /**
+     * The number of relationships in the set that exist on pages after this page.
+     */
+    afterPageCount: number;
+    /**
+     * An array of `SzRelation`'s describing the relationships for the page. The `SzRelation` array will be in ascending order of the first  entity ID and then the second related entity ID.
+     */
+    relations: Array<SzSampleSetRelation>;
+}
+/**
+ * Encapsulates a paged list of entity ID's identifying the entities pertaining to a specific statistic.
+ */
+export interface SzSampleSetEntitiesPage { 
+    /**
+     * The entity ID bound value that bounds the returned entity ID's.
+     */
+    bound: number;
+    boundType: SzBoundType;
+    /**
+     * The requested page size representing the maximum number of  entities that were included in the page.
+     */
+    pageSize: number;
+    /**
+     * The requested sample size representing the number of entities to be randmonly selected from the page of results.
+     */
+    sampleSize?: number;
+    /**
+     * The minimum entity ID of the returned results.  **NOTE:** This field is absent or `null` if there are no results.
+     */
+    minimumValue?: number;
+    /**
+     * The maximum entity ID of the returned results.  **NOTE:** This field is absent or `null` if there are no results.
+     */
+    maximumValue?: number;
+    /**
+     * The minimum entity ID of the entire entity page.  This will  be the same as `minimumValue` if `sampleSize` was not  specified, however, if `sampleSize` was specified then this  will be the minimum entity ID value of all the candidate entities on the page that were used for random sample selection even if that entity was not randomly selected.  **NOTE:** This field is absent or `null` if there are no results.
+     */
+    pageMinimumValue?: number;
+    /**
+     * The maximum entity ID of the entire entity page.  This will  be the same as `maximumValue` if `sampleSize` was not  specified, however, if `sampleSize` was specified then this  will be the maximum entity ID value of all the candidate entities on the page that were used for random sample selection even if that entity was not randomly selected.  **NOTE:** This field is absent or `null` if there are no results.
+     */
+    pageMaximumValue?: number;
+    /**
+     * The total number of entities representing the set of all  possible results across all pages.
+     */
+    totalEntityCount: number;
+    /**
+     * The number of entities in the set that exist on pages before  this page.
+     */
+    beforePageCount: number;
+    /**
+     * The number of entities in the set that exist on pages after this page.
+     */
+    afterPageCount: number;
+    /**
+     * An array of `SzSampleSetEntity` instances describing the entities for the page. The array will be in ascending order of entity ID.
+     */
+    entities: Array<SzSampleSetEntity>;
+}
